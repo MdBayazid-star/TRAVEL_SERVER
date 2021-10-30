bb0c5a7d3ea87c5b4db46a020bdf76f3d2b0e030
MdBayazid-star <freeelencerbayazid@gmail.com>
2021-10-30 20:51:16 +0600

    User Delete

--- index.js
+++ index.js
@@ -65,13 +65,42 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
-
-    // app.get("/users/", async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: ObjectId(id) };
-    //   const user = await usersCollection.findOne(query);
-    //   res.send(user);
-    // });
+    // Get Single Users
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const user = await usersCollection.findOne(query);
+      // console.log('load user with id: ', id);
+      res.send(user);
+    });
+    //Delete Users
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      console.log("deleting user with id ", result);
+      res.json(result);
+    });
+    //Update Users
+    app.put("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedUser = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          name: updatedUser.name,
+          email: updatedUser.email,
+        },
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      console.log("updating", id);
+      res.json(result);
+    });
   } finally {
     // await client.close();
   }
